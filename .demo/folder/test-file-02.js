c89087a53b97e624d53602291ed3a90e9ddc61b0
Daniel Sitek <dan.sitek@gmail.com>
2026-01-24 19:59:37 +0100

    chore: update demo files

---
+++ .demo/folder/test-file-02.js
@@ -0,0 +1,14 @@
+// Demo file for testing ESLint formatters
+const unused = "test";
+
+function example() {
+  const x = 1;
+  console.log("missing semicolon")
+  return x
+}
+
+const y = 'single quotes';
+const x = 'single quotes'
+const v = 'single quotes'
+
+export { example };
